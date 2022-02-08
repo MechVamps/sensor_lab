3251d69458bd652dae057371ceae3e8f4840a51c
Di <di1113@me.com>
2022-02-08 12:51:06 -0500

    Clean up lab1_full.ino and add code for parsing sensor data

--- Lab1_full/Lab1_full.ino
+++ Lab1_full/Lab1_full.ino
@@ -1,5 +1,3 @@
-
-
 // IR VARIABLE DEFINE:=
 /*SHARP GP2Y0A21YK0F IR distance sensor with Arduino and SharpIR library example code. More info: https://www.makerguides.com */
 // Include the library:
@@ -19,108 +17,114 @@ int distance_cm;
 SharpIR mySensor = SharpIR(IRPin, model);
 
 
-
 // AMBIENT TEMPERATURE VARIABLE DEFINE:=
 // Code Source: https://www.tutorialspoint.com/arduino/arduino_temperature_sensor.htm
 // Code Source: https://osoyoo.com/2017/07/22/arduino-lesson-tmp36/
 // Info: https://www.maximintegrated.com/en/products/sensors/MAX31820.html
-float temp;
+int temp;
 int tempPin = 1; // dibs on pin A1
 
-
-
-// FSR VARIABLE DEFINE:=
-const int FSRpin = A2;
-
-const float Vin = 5.0; // voltage (in V) from Arduino Uno
-const float Rm = 10.0; // constant 10 kilo Ohm measuring resistor in circuit
-
-
+//// FSR VARIABLE DEFINE:=
+int fsrPin = 2;     // the FSR and 10K pulldown are connected to a0
+long force; 
 
 void setup() 
 {
-  pinMode(FSRpin, INPUT);
   // Begin serial communication at a baudrate of 9600:
   Serial.begin(9600);
 }
 
 
+//AMBIENT TEMP FUNCTION CODE:=
+double read_temperature(int pin)
+{
+  // convert raw reading to voltage
+  float volt = analogRead(pin) * 5.0;
+  volt /= 1024.0; 
+
+  // converting from 10 mv per degree wit 500 mV offset
+  int temp = (volt - 0.5) * 100;
+   
+  return temp;
+}
+
+long read_force(int pin)
+{
+  int fsrReading;     // the analog reading from the FSR resistor divider
+  int fsrVoltage;     // the analog reading converted to voltage
+  unsigned long fsrResistance;  // The voltage converted to resistance, can be very big so make "long"
+  unsigned long fsrConductance; 
+  long fsrForce;       // Finally, the resistance converted to force
+
+  fsrReading = analogRead(pin);  // get raw counts from FSR
+  Serial.print("Analog reading = ");
+  Serial.println(fsrReading);
+ 
+  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
+  fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+  Serial.print("Voltage reading in mV = ");
+  Serial.println(fsrVoltage);  
+ 
+  if (fsrVoltage == 0) {
+    Serial.println("No pressure");  
+  } else {
+    // The voltage = Vcc * R / (R + FSR) where R = 10K and Vcc = 5V
+    // so FSR = ((Vcc - V) * R) / V        yay math!
+    fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
+    fsrResistance *= 10000;                // 10K resistor
+    fsrResistance /= fsrVoltage;
+    Serial.print("FSR resistance in ohms = ");
+    Serial.println(fsrResistance, 4);
+ 
+    fsrConductance = 1000000;           // we measure in micromhos so 
+    fsrConductance /= fsrResistance;
+    Serial.print("Conductance in microMhos: ");
+    Serial.println(fsrConductance, 4);
+ 
+    // Use the two FSR guide graphs to approximate the force
+    if (fsrConductance <= 1000) {
+      fsrForce = fsrConductance / 80;  
+    } else {
+      fsrForce = fsrConductance - 1000;
+      fsrForce /= 30;          
+    }
+  }
+
+  return fsrForce; 
+}
+
 
 void loop() 
 {
-
   //IR LOOP CODE:=
   // Get a distance measurement and store it as distance_cm:
-  distance_cm = mySensor.getDistance();
+  distance_cm = mySensor.distance();
   // Print the measured distance to the serial monitor:
   Serial.print("Mean distance: ");
   Serial.print(distance_cm);
   Serial.println(" cm");
   delay(1000);
 
-
   //AMBIENT TEMPERATURE LOOP CODE:=
-   // Call function to read temperature
-   temp = read_temperature(tempPin);
-   // Print temp data
-   Serial.print("Temperature [C]: ");
-   Serial.print(temp); // display temperature value
-   Serial.println();
-   delay(1000); // update every 1 sec
+  // Call function to read temperature
+  temp = read_temperature(tempPin);
+  // Print temp data
+  Serial.print("Temperature [C]: ");
+  Serial.print(temp); // display temperature value
+  Serial.println();
+  delay(1000); // update every 1 sec
+  
+  //FSR LOOP CODE:=
+  force = read_force(fsrPin);
+  Serial.print("Force in Newtons: ");
+  Serial.println(force, 4);    
 
-
-   //FSR LOOP CODE:=
-  int FSRcounts = analogRead(FSRpin); // get raw counts from FSR
-//  Serial.println(FSRcounts);
-  if (FSRcounts != 0){
-    float FSRvoltage = FSRcounts*Vin/1023.0; // [V] maps raw analog output to a voltage from 0 to 5V
-    float FSRresistance = Rm*(Vin/FSRvoltage - 1.0); // [kOhms] solve for FSR resistance from output voltage
-    float FSRconductance = 1.0 / FSRresistance; //[1/kOhms] conductance is inverse of resistance
-    float Force; 
-    // 4 Force-Conductance relationships derived from plots in specs.
-    // Different relationship for different conductance ranges.
-    if (FSRconductance <= 0.0025) {
-      Force = 80000*FSRconductance; // [g]
-    }
-    else if  (0.0025<FSRconductance<=0.008) {
-      Force =  145454*FSRconductance - 163.64; // [g]
-    }
-    else if (0.008<FSRconductance<=0.026) {
-      Force = 222222.22*FSRconductance - 777.78; // [g]
-    }
-    else if (0.026<FSRconductance) { // <=0.041
-      Force = 333333.33*FSRconductance - 3666.67; // [g]
-    }
-    else {
-    }
-    Force = 9.81*Force/1000.0; // [N] convert force to Newtons
-    Serial.print("Resistance: ");
-    Serial.print(FSRresistance,4);
-    Serial.print("\t");
-    Serial.print("Conductance: ");
-    Serial.print(FSRconductance,4);
-    Serial.print("\t");
-    Serial.print("Force: ");
-    Serial.println(Force,4);
-  }
-  else{
-    Serial.println("No force detected");
-  }
-    delay(100);
- 
+  Serial.println("--------------------");
+  delay(1000);
+  
+  char buffer[100];
+  sprintf(buffer, "<sensor(T%d;I%d;F%ld)>",temp, distance_cm, force);
+  Serial.println(buffer);
 }
 
 
-
-//AMBIENT TEMP FUNCTION CODE:=
-double read_temperature(int pin)
-{
-   // convert raw reading to voltage
-   float volt = analogRead(pin) * 5.0;
-   volt /= 1024.0; 
-
-   // converting from 10 mv per degree wit 500 mV offset
-   float temp = (volt - 0.5) * 100;
-   
-   return temp;
-}
--- sensor-lab-ui.py
+++ sensor-lab-ui.py
@@ -14,67 +14,68 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        # setup initial value and GUI 
         self.setupUi(self)
-
-        self.x = list(range(100))  # 100 time points
-        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
+        self.temp_value = 0
+        self.ir_value = 0
         self.force_value = 0
-        self.force_PlotView.setBackground('w')
+
+        self.time_axis = list(range(100))  # 100 time points
+        self.temp_data = [0] * 100  
+        self.ir_data = [0] * 100  
+        self.force_data = [0] * 100 
+
+        self.temp_plotView.setBackground('w')
+        self.ir_plotView.setBackground('w')
+        self.force_plotView.setBackground('w')
 
         pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line =  self.force_PlotView.plot(self.x, self.y, pen=pen)
+        self.temp_data_line =  self.temp_plotView.plot(self.time_axis, self.ir_data, pen=pen)
+        self.ir_data_line =  self.ir_plotView.plot(self.time_axis, self.ir_data, pen=pen)
+        self.force_data_line =  self.force_plotView.plot(self.time_axis, self.force_data, pen=pen)
 
         self.serial = QtSerialPort.QSerialPort(
             '/dev/tty.usbmodem141201',
-            # baudRate = QtSerialPort.QSerialPort.Baud9600,     
-            # dataBits = QtSerialPort.QSerialPort.Data8,
-            readyRead=self.receive
+            baudRate = QtSerialPort.QSerialPort.Baud9600,     
+            dataBits = QtSerialPort.QSerialPort.Data8,
+            readyRead = self.receive
         )
-        self.serial.setBaudRate(9600)
         self.serial.open(QtCore.QIODevice.ReadWrite)
 
         self.timer = QtCore.QTimer()
         self.timer.setInterval(50)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
-        
+
+
     @QtCore.pyqtSlot()
     def receive(self):
         while self.serial.canReadLine():
-            text = self.serial.readLine().data().decode()
-            text = text.rstrip('\r\n')
-            force_value_txt = re.findall(r'\d+', text)
-            if force_value_txt != []:
-                self.force_value = int(force_value_txt[0])
-            else: 
-                self.force_value = 0
-            self.force_lcd.setDigitCount(4)
-            self.force_lcd.display(self.force_value)
-            print(self.force_value)
-
-    # @QtCore.pyqtSlot()
-    # def send(self):
-    #     self.serial.write(self.message_le.text().encode())
-
-    # @QtCore.pyqtSlot(bool)
-    # def on_toggled(self, checked):
-    #     self.button.setText("Disconnect" if checked else "Connect")
-    #     if checked:
-    #         if not self.serial.isOpen():
-    #             if not self.serial.open(QtCore.QIODevice.ReadWrite):
-    #                 self.button.setChecked(False)
-    #     else:
-    #         self.serial.close()
+            ino_data = self.serial.readLine().data().decode()
+            ino_text = ino_data.rstrip('\r\n')
+            # ino_text = re.findall(r"<sensor\(T(\d+);I(\d+);F(\d+)\)>", ino_text)
+            ino_data_array = re.findall(r"(\d+)", ino_text)
+            print(ino_data_array)
+            self.temp_value = int(ino_data_array[0])
+            self.ir_value = int(ino_data_array[1])
+            self.force_value = int(ino_data_array[2])
 
     def update_plot_data(self):
 
-        self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+        self.time_axis = self.time_axis[1:]  # Remove the first y element.
+        self.time_axis.append(self.time_axis[-1] + 1)  # Add a new value 1 higher than the last.
 
-        self.y = self.y[1:]  # Remove the first
-        self.y.append(self.force_value)  # Add a new random value.
+        self.temp_data = self.temp_data[1:]  # Remove the first
+        self.temp_data.append(self.temp_value)  # Add a new random value.
+        self.temp_data_line.setData(self.time_axis, self.temp_data)  # Update the data.
 
-        self.data_line.setData(self.x, self.y)  # Update the data.
+        self.ir_data = self.ir_data[1:]  # Remove the first
+        self.ir_data.append(self.ir_value)  # Add a new random value.
+        self.ir_data_line.setData(self.time_axis, self.ir_data)  # Update the data.
+
+        self.force_data = self.force_data[1:]  # Remove the first
+        self.force_data.append(self.force_value)  # Add a new random value.
+        self.force_data_line.setData(self.time_axis, self.force_data)  # Update the data.
 
 
 
